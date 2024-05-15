c53a5c5805056ed635a8e2d3a0a3ad9da42fdc56
Zach <147861361+1054009@users.noreply.github.com>
2024-05-14 21:38:37 -0400

    Setup project to be serverside

--- gm_environment/premake5.lua
+++ gm_environment/premake5.lua
@@ -2,7 +2,7 @@ PROJECT_GENERATOR_VERSION = 3
 
 include("./garrysmod_common/")
 CreateWorkspace({name = "environment", abi_compatible = false, path = "projects/"})
-	CreateProject({serverside = false, source_path = "src", manual_files = false})
+	CreateProject({serverside = true, source_path = "src", manual_files = false})
 		files({
 			"src/**.*"
 		})
